52298e3e9bf16c5e8d29b40106aa75acbece8fed
MahanVarghai <mahanvarghai@gmail.com>
2025-09-20 23:46:42 +0330

    Add project files.

---
+++ LeetCode/1.Two Sum/First_Attempt.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    int buffer;
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            buffer = target - nums[i];
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] == buffer && j != i)
+                    return new int[] { i, j };
+            }
+        }
+        return [];
+    }
+}
